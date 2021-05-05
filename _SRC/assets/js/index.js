4eb54bcd2274343922a6a01dda2f44dc28c9bdb4
T-N9 <tenyainmoelwin@gmail.com>
2021-05-05 21:07:18 +0630

    smNav updated

---
+++ _SRC/assets/js/index.js
@@ -0,0 +1,6 @@
+// UI JS
+document.getElementById('navBurger').onclick=function(){
+    document.getElementById('smNav').classList.toggle('smNav-origin');
+    document.getElementById('smNav').classList.toggle('smNav-active');
+    document.getElementById('navBurger').classList.toggle('navBurger-active');
+}
