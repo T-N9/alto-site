1f4e52bd8d32d52c73681ffc57191d0c7c7eb319
T-N9 <tenyainmoelwin@gmail.com>
2021-05-09 09:08:22 +0630

    spacing updated

--- _SRC/assets/js/index.js
+++ _SRC/assets/js/index.js
@@ -1,6 +1,9 @@
 // UI JS
+new WOW().init();
+
 document.getElementById('navBurger').onclick=function(){
     document.getElementById('smNav').classList.toggle('smNav-origin');
     document.getElementById('smNav').classList.toggle('smNav-active');
     document.getElementById('navBurger').classList.toggle('navBurger-active');
 }
+
