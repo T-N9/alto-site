ad483c104022a4450df2878591da20e52772e9f5
T-N9 <tenyainmoelwin@gmail.com>
2021-05-11 14:17:05 +0630

    Design Updated

--- _SRC/assets/js/index.js
+++ _SRC/assets/js/index.js
@@ -1,12 +1,56 @@
 // UI JS
-$(function(){
-    new WOW().init(); 
+$(document).ready(function () {
+  $(window).on("load", function () {
+    $("#loaderPage").fadeOut(500, function () {
+      this.remove();
+    });
   });
 
+  new WOW().init();
 
-document.getElementById('navBurger').onclick=function(){
-    document.getElementById('smNav').classList.toggle('smNav-origin');
-    document.getElementById('smNav').classList.toggle('smNav-active');
-    document.getElementById('navBurger').classList.toggle('navBurger-active');
-}
+  let screenHeight = $(window).height();
+  $(window).scroll(function () {
+    let currentPos = $(this).scrollTop();
+  });
 
+  $("#navBurger").click(function () {
+    $("#smNav").toggleClass("smNav-origin");
+    $("#smNav").toggleClass("smNav-active");
+    $("#navBurger").toggleClass("navBurger-active");
+  });
+  $(".nav-link").click(function () {
+    $("#smNav").toggleClass("smNav-origin");
+    $("#smNav").toggleClass("smNav-active");
+    $("#navBurger").removeClass("navBurger-active");
+  });
+
+  function navActive(current) {
+    $(`.nav-link`).removeClass("nav-active");
+    $(`.nav-link[href='#${current}']`).addClass("nav-active");
+  }
+  function navScroll() {
+    let currentSec = $("section[id]");
+    currentSec.waypoint(
+      function (direction) {
+        if (direction == "down") {
+          let currentId = $(this.element).attr("id");
+          // console.log(currentId);
+          navActive(currentId);
+        }
+      },
+      { offset: "10px" }
+    );
+    currentSec.waypoint(
+      function (direction) {
+        if (direction == "up") {
+          let currentId = $(this.element).attr("id");
+          // console.log(currentId);
+          navActive(currentId);
+        }
+      },
+      { offset: "-10px" }
+    );
+  }
+
+  navScroll();
+});
