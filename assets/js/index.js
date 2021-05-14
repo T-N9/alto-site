ede1d97c7fb4106644f1f68f9a144d1ba0a95854
Alto Tun Chit Oo <altotunchitoo@icloud.com>
2021-05-15 01:20:53 +0630

    [Alto] update dev page UI UX

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,30 @@
 // UI JS
-$(document).ready(function () {
-  $(window).on("load", function () {
-    $("#loaderPage").fadeOut(500, function () {
-      this.remove();
+$(document).ready(function() {
+    $(window).on("load", function() {
+        $("#loaderPage").fadeOut(500, function() {
+            this.remove();
+        });
+        $('#title').addClass('title-animate');
     });
-    $('#title').addClass('title-animate');
-  });
 
-  let screenHeight = $(window).height();
-  $(window).scroll(function () {
-    let currentPos = $(this).scrollTop();
+    let screenHeight = $(window).height();
+    $(window).scroll(function() {
+        let currentPos = $(this).scrollTop();
 
-    if(currentPos>=screenHeight-600){
-      $('#navBar').removeClass('nav-none');
-      $('#navBar').addClass('nav-income');
-      $('#smNav').removeClass('nav-none');
-      $('#smNav').addClass('nav-income');
-    }
-    else{
-      $('#navBar').addClass('nav-none');
-      $('#navBar').removeClass('nav-income');
-      $('#smNav').addClass('nav-none');
-      $('#smNav').removeClass('nav-income');
-    }
-  });
+        if (currentPos >= screenHeight - 600) {
+            $('#navBar').removeClass('nav-none');
+            $('#navBar').addClass('nav-income');
+            $('#smNav').removeClass('nav-none');
+            $('#smNav').addClass('nav-income');
+        } else {
+            $('#navBar').addClass('nav-none');
+            $('#navBar').removeClass('nav-income');
+            $('#smNav').addClass('nav-none');
+            $('#smNav').removeClass('nav-income');
+        }
+    });
 
-  
+
     // else{
     //     $('.site-nav').removeClass('site-nav-scroll');
     //     navActive('home');
@@ -33,48 +32,48 @@ $(document).ready(function () {
     //     $("#up-link").addClass('d-none');
     // }
 
-  new WOW().init();
+    new WOW().init();
 
-  
 
-  $("#navBurger").click(function () {
-    $("#smNav").toggleClass("smNav-origin");
-    $("#smNav").toggleClass("smNav-active");
-    $("#navBurger").toggleClass("navBurger-active");
-  });
-  $(".nav-link").click(function () {
-    $("#smNav").toggleClass("smNav-origin");
-    $("#smNav").toggleClass("smNav-active");
-    $("#navBurger").removeClass("navBurger-active");
-  });
 
-  function navActive(current) {
-    $(`.nav-link`).removeClass("nav-active");
-    $(`.nav-link[href='#${current}']`).addClass("nav-active");
-  }
-  function navScroll() {
-    let currentSec = $("section[id]");
-    currentSec.waypoint(
-      function (direction) {
-        if (direction == "down") {
-          let currentId = $(this.element).attr("id");
-          // console.log(currentId);
-          navActive(currentId);
-        }
-      },
-      { offset: "10px" }
-    );
-    currentSec.waypoint(
-      function (direction) {
-        if (direction == "up") {
-          let currentId = $(this.element).attr("id");
-          // console.log(currentId);
-          navActive(currentId);
-        }
-      },
-      { offset: "-10px" }
-    );
-  }
+    $("#navBurger").click(function() {
+        $("#smNav").toggleClass("smNav-origin");
+        $("#smNav").toggleClass("smNav-active");
+        $("#navBurger").toggleClass("navBurger-active");
+    });
+    $(".nav-link").click(function() {
+        $("#smNav").toggleClass("smNav-origin");
+        $("#smNav").toggleClass("smNav-active");
+        $("#navBurger").removeClass("navBurger-active");
+    });
 
-  navScroll();
+    function navActive(current) {
+        $(`.nav-link`).removeClass("nav-active");
+        $(`.nav-link[href='#${current}']`).addClass("nav-active");
+    }
+
+    function navScroll() {
+        let currentSec = $("section[id]");
+        currentSec.waypoint(
+            function(direction) {
+                if (direction == "down") {
+                    let currentId = $(this.element).attr("id");
+                    // console.log(currentId);
+                    navActive(currentId);
+                }
+            }, { offset: "10px" }
+        );
+        currentSec.waypoint(
+            function(direction) {
+                if (direction == "up") {
+                    let currentId = $(this.element).attr("id");
+                    // console.log(currentId);
+                    navActive(currentId);
+                }
+            }, { offset: "-10px" }
+        );
+    }
+    navScroll();
+
+    $('#dev').click(_ => window.open("https://play.google.com/store/apps/dev?id=5596247171368474827", "_blank"));
 });
